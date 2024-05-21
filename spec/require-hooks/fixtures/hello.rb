50f52d94b887cee5422b41bf3ccd0f91431d09e9
Vladimir Dementyev <palkan@hey.com>
2024-05-21 16:59:22 -0700

    fix: update frozen check for Ruby 3.4

--- spec/require-hooks/fixtures/hello.rb
+++ spec/require-hooks/fixtures/hello.rb
@@ -1,2 +1,14 @@
+# Ruby 3.4 introduced chilled strings that
+# pretend to be frozen (i.e., responds with true to #frozen?).
+# That will probably change in the final release though:
+#   https://bugs.ruby-lang.org/issues/20205#note-45
+def frozen?(str)
+  str.sub!(/$/, "x")
+  str.sub!(/x$/, "")
+  false
+rescue FrozenError
+  true
+end
+
 str = "Hello"
-puts str + " (#{str.frozen?})"
+puts str + " (#{frozen?(str)})"
