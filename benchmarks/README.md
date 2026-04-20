9c0199464eefb25a26aa07ca1b90c27f3e40e220
Vladimir Dementyev <dementiev.vm@gmail.com>
2026-04-21 00:21:47 +0900

    + benchmarks

---
+++ benchmarks/README.md
@@ -0,0 +1,43 @@
+# Benchmarks
+
+Purpose: measure the overhead of using require-hooks compared to not having hooks at all in various modes.
+
+1. Setup the file structure:
+
+```sh
+ruby setup.rb 24
+```
+
+The command above creates a `project` directory with the nested Ruby files structure and the `project/project.rb` entrypoint that loads all the files. The argument (24) is the size of the first level of the nesting; at each next level the nesting decreases by 2x. So, for example, for 24, the total number of `require` calls is 12408 (24 + 24x12 + 24x12x6 + 24x12x6x3 + 24x12x6x3x1).
+
+Every time you run the `setup.rb` script, the project folder is re-created.
+
+2. Load the test project:
+
+```sh
+# no hooks
+ruby project/project.rb
+
+# with Bootsnap (don't forget to warmup the cache)
+BOOTSNAP=1 ruby project/project.rb
+
+# with require-hooks enabled but no hooks
+HOOKS=1 ruby project/project.rb
+
+# with an around hook that tracks the number of requires
+$ HOOKS=around ruby project/project.rb
+
+Total requires: 12408
+``
+
+3. Use hyperfine to run benhchmarks:
+
+```sh
+hyperfine --warmup 2 'ruby project/project.rb' 'HOOKS=1 ruby project/project.rb' 'HOOKS=around ruby project/project.rb'
+
+# with bootsnap
+hyperfine --warmup 2 'BOOTSNAP=1 ruby project/project.rb' 'BOOTSNAP=1 HOOKS=1 ruby project/project.rb' 'BOOTSNAP=1 HOOKS=around ruby project/project.rb'
+
+# load_iseq vs. kernel patch
+hyperfine --warmup 2 'REQUIRE_HOOKS_MODE=load_iseq HOOKS=around ruby project/project.rb' 'REQUIRE_HOOKS_MODE=patch HOOKS=around ruby project/project.rb'
+```
---
+++ benchmarks/setup.rb
@@ -0,0 +1,106 @@
+# This setup script creates a project structure with thousands of files to require and a single entrypoint.rb file
+# to recursively load them all
+#
+# project/
+#   entrypoint.rb
+#   a/
+#    a/
+#      a.rb
+#      b.rb
+#      ...
+#      h.rb
+#    b/
+#    ...
+#    m/
+#    a.rb
+#   b/
+#   ...
+#   z/
+#
+# The first hierarchy level contains N folders, each next one has N/2 folders. There are log(N) levels.
+
+require "fileutils"
+
+N = ARGV[0]&.to_i || 100
+
+def create_directory(n, prefix: nil, namespace: "")
+  name = "a"
+  nested = n / 2
+  n.times do
+    FileUtils.mkdir(name)
+    nested_name = "a"
+    nested_requires = Array.new(nested).map do
+      %(require "#{prefix}#{name}/#{nested_name}").tap do
+        nested_name = nested_name.succ
+      end
+    end
+
+    File.write("#{name}.rb", <<~RUBY)
+      module #{namespace}::#{name.capitalize}
+        def foo
+          "#{prefix}-bar"
+        end
+
+        attr_reader *("a".."z").to_a
+
+        ("a".."z").each do |v|
+          define_method(:"foo_\#{v}") do |input|
+            input + " " + v
+          end
+        end
+      end
+
+      #{nested_requires.join("\n")}
+
+      module #{namespace}::#{name.capitalize}
+        include #{namespace}::#{name.capitalize}::A if defined?(include #{namespace}::#{name.capitalize}::A)
+      end
+    RUBY
+
+    Dir.chdir(name) do
+      create_directory(nested, prefix: "#{prefix}#{name}/", namespace: "#{namespace}::#{name.capitalize}")
+    end
+    name = name.succ
+  end
+end
+
+root_dir = File.expand_path("project", __dir__)
+FileUtils.rm_r(root_dir) if File.directory?(root_dir)
+FileUtils.mkdir(root_dir)
+
+Dir.chdir(root_dir) do
+  create_directory(N)
+
+  top_name = "a"
+  top_requires = Array.new(N).map do
+    %(require "#{top_name}").tap do
+      top_name = top_name.succ
+    end
+  end
+
+  File.write("project.rb", <<~RUBY
+    $LOAD_PATH.unshift(File.join(__dir__, "../lib"))
+    $LOAD_PATH.unshift(__dir__)
+
+    if ENV["BOOTSNAP"]
+      ENV["BOOTSNAP_CACHE_DIR"] = File.expand_path("tmp/bootsnap", __dir__)
+      require "bootsnap/setup"
+    end
+
+    if ENV["HOOKS"]
+      require "require-hooks/setup"
+
+      if ENV["HOOKS"].include?("around")
+        $counter = 0
+        RequireHooks.around_load(patterns: ["\#{__dir__}/*.rb"]) do |path, &block|
+          block.call.tap { $counter += 1 }
+        end
+
+        at_exit { puts "Total requires: \#{$counter}" }
+      end
+    end
+
+    #{top_requires.join("\n")}
+  RUBY
+  )
+end
